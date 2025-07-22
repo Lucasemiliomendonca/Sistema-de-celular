bf7fc2fa876a41e561683e7130c4371b86f80cb4
Lucasemiliomendonca <mendoncae.metrocasa@gmail.com>
2025-07-21 23:04:12 -0300

    Desafio concluído: Sistema de Celular!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "11999999999", modelo: "Iphone XV", imei: "246515646651", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "12999999999", modelo: "Nokia 16", imei: "123875316358", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
